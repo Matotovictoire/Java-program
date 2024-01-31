0789e119be5ace827cae94a559321db5bd56b469
matotovictoire <matotovictoire@gmail.com>
2024-01-31 12:57:53 +0200

    Write a  program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.

---
+++ multiplecalc.java
@@ -0,0 +1,17 @@
+public class multiplecalc {
+   public static void main(String[] args) {
+    int a = 5;
+    int b = 5;
+    int c = a+b;
+    int d = a*b;
+    int e = a-b;
+    int f = a/b;
+    int g = a%b;
+    System.out.println("the first response is "+c);
+    System.out.println("\nthe second one is: "+d);
+    System.out.println("\nthe third is: "+e);
+    System.out.println("\nThe fourth one is:"+f);
+    System.out.println("\nthe fifth one"+g);
+
+   } 
+}
