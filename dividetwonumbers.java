6fad891d1c2c8c9e9176f4e1c1246e2e70041386
matotovictoire <matotovictoire@gmail.com>
2024-01-31 12:52:37 +0200

    Write a   program to divide two numbers and print them on the screen

---
+++ dividetwonumbers.java
@@ -0,0 +1,9 @@
+public class dividetwonumbers {
+    public static void main(String[] args) {
+        // Divide two numbers
+        int a=23; // the declaration of the first number
+        int b=3; // the declaration of the second number
+        int d=a/b; // the calculation of that division
+        System.out.println(d);
+    }
+}
