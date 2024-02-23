cf316975d153f16943556c88e8f27f86c22134ce
matotovictoire <matotovictoire@gmail.com>
2024-02-23 15:06:28 +0200

    This is all about the code in java including some program using some networking skills

--- net2.java
+++ net2.java
@@ -5,7 +5,7 @@ public class net2 {
         // The program to print the ip of a given hostname
         try{InetAddress ip = InetAddress.getByName("auca.ac.rw");
         System.out.println("The Host ip is: " +ip);
-        System.out.println("auca.ac.rw/197.243.26.123");
+      //  System.out.println("auca.ac.rw/197.243.26.123");
     }catch (Exception e) {System.out.println(e);}
     }
 }
